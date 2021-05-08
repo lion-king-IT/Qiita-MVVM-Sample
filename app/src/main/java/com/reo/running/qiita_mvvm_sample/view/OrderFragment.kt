d778eca9d4bc61d3c08bbea814c4d34745aaaffc
github lion-king-IT <reo.runningg@gmail.com>
2021-05-08 14:53:02 +0900

    画像の更新が上手くいかない。

--- app/src/main/java/com/reo/running/qiita_mvvm_sample/view/OrderFragment.kt
+++ app/src/main/java/com/reo/running/qiita_mvvm_sample/view/OrderFragment.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.reo.running.qiita_mvvm_sample.databinding.FragmentOrderBinding
 import com.reo.running.qiita_mvvm_sample.viewModel.OrderViewModel
 
 class OrderFragment : Fragment() {
-    private val viewModel: OrderViewModel by activityViewModels()
+
+    // 客席にタッチパネル設置
+    private val viewModel: OrderViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +22,22 @@ class OrderFragment : Fragment() {
     ): View? = FragmentOrderBinding.inflate(inflater, container, false).let {
         it.lifecycleOwner = viewLifecycleOwner
         it.orderViewModel = viewModel
-        it.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val orderObserver = Observer<String> { _ ->
+        // 監視センサ作成
+        val imageObserver = Observer<Int> { newImageId ->
+            // ImageViewの更新処理
+            it.orderImageView.setImageResource(newImageId)
         }
+
+        // 監視センサ作成
+        val textObserver = Observer<String> { newText ->
+            // TextViewの更新処理
+            it.tunaOrderButton.text = newText
+        }
+
+        // viewLifecycleOwnerを第一引数年て渡すことで、お客さんに合わせて監視する設定
+        viewModel.orderImage.observe(viewLifecycleOwner, imageObserver)
+        viewModel.orderText.observe(viewLifecycleOwner, textObserver)
+        it.root
     }
 }
--- app/src/main/java/com/reo/running/qiita_mvvm_sample/viewModel/OrderViewModel.kt
+++ app/src/main/java/com/reo/running/qiita_mvvm_sample/viewModel/OrderViewModel.kt
@@ -1,18 +1,47 @@
 package com.reo.running.qiita_mvvm_sample.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.reo.running.qiita_mvvm_sample.R
 
 class OrderViewModel : ViewModel() {
-    // ボタンテキストの状態と画像の状態を保持
-    val orderImage: MutableLiveData<Int> by lazy {
-        MutableLiveData<Int>()
+
+    // 画像のidを保持
+    private val _orderImage = MutableLiveData<Int>() // 板前さんだけが持つ
+    val orderImage: LiveData<Int> // お客さんに渡す方、変更できない
+        get() = _orderImage
+
+    // 注文ボタンのテキストの状態を保持
+    private val _orderText = MutableLiveData<String>()
+    val orderText: LiveData<String>
+        get() = _orderText
+
+    // 会計ボタンのテキストを保持
+    private val _billText = MutableLiveData<String>()
+    val billText: LiveData<String>
+        get() = _billText
+
+    init {
+        _orderText.value = "マグロ"
+        _billText.value = "会計"
+        _orderImage.value = R.drawable.sushi_syokunin_man_mask
     }
+
+    // 注文する処理
+    fun orderTuna() {
+        if (_orderText.value.equals("マグロ")) {
+            _orderImage.value = R.drawable.sushi_akami
+            _orderText.value = "完食"
+        } else {
+            _orderImage.value = R.drawable.sushi_syokunin_man_mask
+            _orderText.value = "マグロ"
+        }
+    }
+
+    // お会計処理
+    fun pay() {
+        _orderImage.value = R.drawable.message_okaikei_ohitori
+    }
+
 }
-
-/*
-
-
-
-
- */
