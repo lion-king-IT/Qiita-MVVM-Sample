72b2122a471b77428feb18d49e14f5f5d695e619
github lion-king-IT <reo.runningg@gmail.com>
2021-05-11 19:01:00 +0900

    引数エラーになる

--- app/src/main/java/com/reo/running/qiita_mvvm_sample/view/MainActivity.kt
+++ app/src/main/java/com/reo/running/qiita_mvvm_sample/view/MainActivity.kt
@@ -3,12 +3,14 @@ package com.reo.running.qiita_mvvm_sample.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.reo.running.qiita_mvvm_sample.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         val orderFragment = OrderFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.fragmentContainer, orderFragment)
