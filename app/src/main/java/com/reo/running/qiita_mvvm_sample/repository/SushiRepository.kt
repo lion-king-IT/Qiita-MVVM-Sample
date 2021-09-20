a7e055fa4a526fec06193065daf81b32a08bea3b
reoru <reo.runningg@gmail.com>
2021-09-20 17:49:21 +0900

    add repository

--- app/src/main/java/com/reo/running/qiita_mvvm_sample/repository/SushiRepository.kt
+++ app/src/main/java/com/reo/running/qiita_mvvm_sample/repository/SushiRepository.kt
@@ -1,6 +1,27 @@
 package com.reo.running.qiita_mvvm_sample.repository
 
-import com.reo.running.qiita_mvvm_sample.MyApplication
+import android.content.Context
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import javax.inject.Inject
 
-class SushiRepository() {
+class SushiRepository @Inject constructor(
+    private val context: Context
+) {
+    suspend fun getSushiList(): List<String> = withContext(Dispatchers.IO) {
+        val resources = context.resources
+        val assetManager = resources.assets //アセット呼び出し
+        val inputStream = assetManager.open("sushi.json") //Jsonファイル
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+
+        val gson = Gson()
+        gson.fromJson(bufferedReader, Tmp::class.java).data
+    }
 }
+
+class Tmp(
+    val data: List<String>
+)
